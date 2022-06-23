5141c7b6f8915c32962db2439617dc8a5f377d67
Ethan Holz <iplaybassx2@gmail.com>
2022-06-22 19:40:11 -0500

    Converted from array to singly-linked list

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+var albums = List{nil}
+
 type album struct {
     ID      int  `json:"id"`
     Title   string  `json:"title"`
@@ -13,14 +15,59 @@ type album struct {
     Price   float64 `json:"price"`
 }
 
-var albums = []album {
-    {ID: 1, Title: "Surf", Artist: "Surfaces", Price: 15.7},
-    {ID: 2, Title: "Surf 2", Artist: "Surfaces", Price: 15.76},
+type Node struct {
+    album album
+    next *Node
+}
+
+type List struct {
+    head *Node
+}
+
+func (L *List) insert(album album){
+    list := &Node {
+        album: album,
+        next: L.head,
+    }
+    L.head = list
+}
+
+func (L *List) getLength() int {
+    length := 0
+    start := L.head
+    length++
+    for start != nil {
+        start = start.next
+        length++
+    }
+    return length
+}
+
+func (L *List) getNodeById(id int) *Node {
+    start := L.head
+    for start != nil {
+        if start.album.ID == id {
+            return start
+        }
+        start = start.next
+    }
+    return nil
+}
+
+func (L *List) convertToList() []album {
+    var albumArray []album
+    start := L.head
+    for start != nil {
+        albumArray = append(albumArray, start.album)
+        start = start.next
+    }
+    return albumArray
 }
 
 
+
 func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
+    c.IndentedJSON(http.StatusOK, albums.convertToList())
 }
 
 func addAlbum(c *gin.Context) {
@@ -30,28 +77,34 @@ func addAlbum(c *gin.Context) {
     if err := c.BindJSON(&newAlbum); err != nil {
         return
     }
-
+    length := albums.getLength()
     // Always increment the ID based on length rather than
     // using specified
-    if newAlbum.ID != len(albums) + 1 {
-        newAlbum.ID = len(albums) + 1
+    if newAlbum.ID != length {
+        newAlbum.ID = length
     }
-    albums = append(albums, newAlbum)
+    albums.insert(newAlbum)
     c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 func getAlbumById(c *gin.Context) {
     id := c.Param("id")
     int_id, _ := strconv.Atoi(id)
-    int_id -= 1
-    if int_id <= len(albums) - 1 {
-        c.IndentedJSON(http.StatusOK, albums[int_id])
-        return
+    if int_id <= albums.getLength() {
+        albumNode := albums.getNodeById(int_id)
+        if albumNode != nil {
+            c.IndentedJSON(http.StatusOK, &albumNode.album)
+            return
+        }
     }
     c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 func main() {
+    var album1 = album{ID: 1, Title: "Surf", Artist: "Surfaces", Price: 15.7}
+    var album2 = album{ID: 2, Title: "Surf2", Artist: "Surfaces", Price: 15.7}
+    albums.insert(album1)
+    albums.insert(album2)
     router := gin.Default()
     router.GET("/albums", getAlbums)
     router.POST("/albums", addAlbum)
